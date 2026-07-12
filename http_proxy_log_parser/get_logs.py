6900595d374fb40cfb3d4f842ce717eb0d3aaf06
Oskar Pawliszak <oskarpawliszak@macbook.local>
2026-07-12 11:49:11 +0200

    refactor

--- http_proxy_log_parser/get_logs.py
+++ http_proxy_log_parser/get_logs.py
@@ -26,7 +26,7 @@ def get_logs():
                     name=pod_name, 
                     namespace=ingress_namespace, 
                     container=container_name, 
-                    tail_lines=10000)
+                    tail_lines=100)
 
                 container_logs = logs.strip("b'\"").encode().decode("unicode_escape").splitlines()
 
@@ -36,115 +36,46 @@ def get_logs():
                     
                     if parsed_log == None:
                         continue
+                    def count_by_occurence(log, metric, data_dict):
+                        my_metric = log[metric]
+                        my_metric_in_data_dict = data_dict.get(my_metric)
 
-                    # Top n IP Addresses by request count
-                    ClientHost = parsed_log["ClientHost"]
-                    
-                    client_ip_hits = ip_hits.get(ClientHost)
-                    is_client_in_ip_hits = bool(client_ip_hits)
+                        is_my_metric_in_data_dict = bool(my_metric_in_data_dict)
 
-                    if is_client_in_ip_hits:
-                        updated_client_ip_hits = client_ip_hits + 1
-                        ip_hits.update(
-                            {ClientHost: updated_client_ip_hits}
-                        )
-                    else:
-                        ip_hits.update({
-                            ClientHost: 1
-                        })
-
-                    # Request count per status code
-                    DownstreamStatus = parsed_log["DownstreamStatus"]
-
-                    status_count = statuses_count.get(DownstreamStatus)
-                    is_status_in_status_count = bool(status_count)
-
-                    if is_status_in_status_count:
-                        updated_status_count = status_count + 1
-                        statuses_count.update(
-                            {DownstreamStatus: updated_status_count}
-                        )
-                    else:
-                        statuses_count.update({
-                            DownstreamStatus: 1
-                        })
-
-                    # Requested Endpoints
-                    RequestPath = parsed_log["RequestPath"]
-
-                    request_path = request_path_count.get(RequestPath)
-                    is_request_path_in_request_path_count = bool(request_path)
-
-                    if is_request_path_in_request_path_count:
-                        updated_request_path = request_path + 1
-                        request_path_count.update(
-                            {RequestPath: updated_request_path}
-                        )
-                    else:
-                        request_path_count.update({
-                            RequestPath: 1
-                        })
-                    # Request address     
-                    RequestAddr= parsed_log["RequestAddr"]
-
-                    request_addr = request_addr_count.get(RequestAddr)
-                    is_request_addr_in_request_addr_count = bool(request_addr)
-
-                    if is_request_addr_in_request_addr_count:
-                        updated_request_addr = request_addr + 1
-                        request_addr_count.update(
-                            {RequestAddr: updated_request_addr}
-                        )
-                    else:
-                        request_addr_count.update({
-                            RequestAddr: 1
-                        })
-
-
-
+                        if is_my_metric_in_data_dict:
+                            updated_my_metric = my_metric_in_data_dict + 1
+                            data_dict.update(
+                                {my_metric: updated_my_metric}
+                            )
+                        else:
+                            data_dict.update({
+                                my_metric: 1
+                            })
+                    count_by_occurence(parsed_log, "ClientHost", ip_hits)
+                    count_by_occurence(parsed_log, "DownstreamStatus", statuses_count)
+                    count_by_occurence(parsed_log, "RequestPath", request_path_count)
+                    count_by_occurence(parsed_log, "RequestAddr", request_addr_count)
 
             except Exception as e:
                 print(f"Error getting logs for pod {pod_name}, container {container_name}: {e}")
  
 
-    def get_second_value(item):
-        return item[1]
-    
     #IP HITS
-    sorted_ip_hits = sorted(ip_hits.items(), key=get_second_value, reverse=True)
+    def show_top_metrics(message, dict_items):
+        print(message)
+        print("-----------------------")
 
-    print("TOP 10 HIT COUNTS IP...")
-    print("-----------------------")
-    
-    for i,entry in enumerate(sorted_ip_hits, start=1):
-        if i <= 10:
-            print(f'{i}. {entry[0]:<16} | {entry[1]} {"hit" if entry[1] == 1 else "hits"}')
 
-    # STATUS CODES
-    sorted_statuses_count = sorted(statuses_count.items(), key=get_second_value, reverse=True)
-    print("\n")
-    print("TOP STATUS CODES...")
-    print("-----------------------")
-    
-    for i,entry in enumerate(sorted_statuses_count, start=1):
-        print(f'{i}. {entry[0]:<6} | {entry[1]}')
-    
-    # REQUEST PATHS
-    sorted_request_path_count = sorted(request_path_count.items(), key=get_second_value, reverse=True)
-    print("\n")
-    print("TOP 10 Request Paths...")
-    print("-----------------------")
-    
-    for i,entry in enumerate(sorted_request_path_count, start=1):
-        if i <= 10:
-            print(f'{i}. {entry[0]:<6} | {entry[1]}')
+        def get_second_value(item):
+            return item[1]
+        
+        sorted_dict_items = sorted(dict_items.items(), key=get_second_value, reverse=True)
 
-    # REQUEST ADDR
-    sorted_request_addr_count = sorted(request_addr_count.items(), key=get_second_value, reverse=True)
-    print("\n")
-    print("TOP 10 Request addresses...")
-    print("-----------------------")
-    
-    for i,entry in enumerate(sorted_request_addr_count, start=1):
-        if i <= 10:
-            print(f'{i}. {entry[0]:<6} | {entry[1]}')
+        for i,entry in enumerate(sorted_dict_items, start=1):
+            if i <= 10:
+                print(f'{i}. {entry[0]:<16} | {entry[1]}')
+
+    show_top_metrics("TOP 10 HIT COUNTS IP...",ip_hits)
+    show_top_metrics("TOP 10 STATUS CODES...",statuses_count)
+    show_top_metrics("TOP 10 Request Paths...",request_path_count)
+    show_top_metrics("TOP 10 Request addresses...",request_addr_count)
