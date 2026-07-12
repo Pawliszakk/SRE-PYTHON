623415591aa0df93496b2718ae3f2f3742e0f794
Oskar Pawliszak <oskarpawliszak@macbook.local>
2026-07-12 12:04:12 +0200

    before refactor

--- http_proxy_log_parser/get_logs.py
+++ http_proxy_log_parser/get_logs.py
@@ -1,16 +1,7 @@
-from log_parser import log_parser
-from utils import show_top_metrics, count_by_occurence
 from connect_to_k8s import connect_to_k8s
-def get_logs():
 
-    ingress_namespace = "kube-system"
-    ingress_labels = "app.kubernetes.io/name=rke2-traefik"
-    ip_hits = {}
-    statuses_count = {}
-    request_path_count = {}
-    request_addr_count = {}
+def get_logs(ingress_namespace, ingress_labels):
 
-    
     v1 = connect_to_k8s(ingress_namespace, ingress_labels)
 
     pods = v1.list_namespaced_pod(
@@ -31,22 +22,9 @@ def get_logs():
 
                 container_logs = logs.strip("b'\"").encode().decode("unicode_escape").splitlines()
 
-
-                for log in container_logs:
-                    parsed_log = log_parser(log)
-                    
-                    if parsed_log == None:
-                        continue
-
-                    count_by_occurence(parsed_log, "ClientHost", ip_hits)
-                    count_by_occurence(parsed_log, "DownstreamStatus", statuses_count)
-                    count_by_occurence(parsed_log, "RequestPath", request_path_count)
-                    count_by_occurence(parsed_log, "RequestAddr", request_addr_count)
+                return container_logs
 
             except Exception as e:
                 print(f"Error getting logs for pod {pod_name}, container {container_name}: {e}")
 
-    show_top_metrics("TOP 10 HIT COUNTS IP...",ip_hits)
-    show_top_metrics("TOP 10 STATUS CODES...",statuses_count)
-    show_top_metrics("TOP 10 Request Paths...",request_path_count)
-    show_top_metrics("TOP 10 Request addresses...",request_addr_count)
+
--- http_proxy_log_parser/main.py
+++ http_proxy_log_parser/main.py
@@ -1,7 +1,32 @@
 from get_logs import get_logs
+from utils import show_top_metrics, count_by_occurence
+from log_parser import log_parser
 
 def main():
-    logs = get_logs()
 
+    ingress_namespace = "kube-system"
+    ingress_labels = "app.kubernetes.io/name=rke2-traefik"
+    ip_hits = {}
+    statuses_count = {}
+    request_path_count = {}
+    request_addr_count = {}
+    
+    logs = get_logs(ingress_namespace, ingress_labels)
+    
+    for log in logs:
+        parsed_log = log_parser(log)
+        
+        if parsed_log == None:
+            continue
+
+        count_by_occurence(parsed_log, "ClientHost", ip_hits)
+        count_by_occurence(parsed_log, "DownstreamStatus", statuses_count)
+        count_by_occurence(parsed_log, "RequestPath", request_path_count)
+        count_by_occurence(parsed_log, "RequestAddr", request_addr_count)
+    
+    show_top_metrics("TOP 10 HIT COUNTS IP...",ip_hits)
+    show_top_metrics("TOP 10 STATUS CODES...",statuses_count)
+    show_top_metrics("TOP 10 Request Paths...",request_path_count)
+    show_top_metrics("TOP 10 Request addresses...",request_addr_count)
 if __name__ == "__main__":
     main()
