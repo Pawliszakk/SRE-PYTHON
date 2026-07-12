790a0daeb409a7e5981151d2d38e7fc7b1454fa1
Oskar Pawliszak <oskarpawliszak@macbook.local>
2026-07-12 12:13:19 +0200

    big refactor

--- http_proxy_log_parser/main.py
+++ http_proxy_log_parser/main.py
@@ -7,6 +7,7 @@ def main():
     ingress_namespace = "kube-system"
     ingress_labels = "app.kubernetes.io/name=rke2-traefik"
     log_lines = 100000
+    log_count = 0
     ip_hits = {}
     statuses_count = {}
     request_addr_count = {}
@@ -15,6 +16,8 @@ def main():
     logs = get_logs(ingress_namespace, ingress_labels, log_lines)
 
     for log in logs:
+        log_count += 1
+
         parsed_log = log_parser(log)
         
         if parsed_log is None:
@@ -25,6 +28,7 @@ def main():
         count_by_occurence(parsed_log, "RequestAddr", request_addr_count)
         count_by_occurence(parsed_log, "RequestPath", request_path_count)
     
+    print(f"Log count: {log_count}")
     show_top_metrics("TOP 10 IP hit counts...",ip_hits)
     show_top_metrics("TOP 10 Status codes...",statuses_count)
     show_top_metrics("TOP 10 Request addresses...",request_addr_count)
