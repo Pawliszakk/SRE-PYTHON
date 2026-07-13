cdc2081414356e061cc7bf79b0246a663c3ef4a3
o.pawliszak <o.pawliszak@pl.mw-wheels.com>
2026-07-13 16:05:50 +0200

    sandbox, main changes to custom ip

--- sandbox/args_program.py
+++ sandbox/args_program.py
@@ -1,6 +1,19 @@
 import argparse
-parser = argparse.ArgumentParser(description="Test argparse")
-parser.add_argument("--log-lines", type=int, default=100, help="Number of log lines")
+
+parser = argparse.ArgumentParser(
+    description='Script that tests the arguments',
+    epilog='''
+EXAMPLES:
+  py args_program.py --testo hello --testo1 world
+  py args_program.py --testo foo
+''',
+    formatter_class=argparse.RawDescriptionHelpFormatter
+)
+
+parser.add_argument('--testo', type=str, help='A first variable to test argument adding')
+parser.add_argument('--testo1', type=str, help='A sec variable to test argument adding')
 
 args = parser.parse_args()
-print(args.log_lines)
+
+print(f'testo: {args.testo}')
+print(f'testo1: {args.testo1}')
--- traefik_parser/main.py
+++ traefik_parser/main.py
@@ -9,7 +9,7 @@ def main():
 
     ingress_namespace = "kube-system"
     ingress_labels = "app.kubernetes.io/name=rke2-traefik"
-    log_lines = 1000
+    log_lines = 100000
     log_count = 0
     bad_log_count = 0
     ip_hits = {}
@@ -20,6 +20,8 @@ def main():
     all_durations_values = []
     logs = get_logs(ingress_namespace, ingress_labels, log_lines)
 
+    custom_ip = "10.1.10.253"
+
     for log in logs:
         log_count += 1
 
@@ -28,22 +30,31 @@ def main():
         if parsed_log is None:
             bad_log_count += 1
             continue
-    
-        for status_count in statuses_count.items():
-            if status_count[0] >= 500 and status_count[0] <=599:
-                bad_statuses_count.update({
-                    status_count[0]: status_count[1]
-                })
 
         if ("follow=true" not in parsed_log["RequestPath"] # Follow requests case
             and "rancher" not in parsed_log["RequestAddr"] # Rancher live logs fetching case
             and parsed_log["Duration"] < MAX_REASONABLE_DURATION): # Follow requests case + not normal requests that are bug probably
                 all_durations_values.append(parsed_log["Duration"])
 
+        # 5xx logs
+        # if parsed_log["DownstreamStatus"] >= 500 and parsed_log["DownstreamStatus"] <=599:
+        #      print(parsed_log)
+        
+        # Logs By IP 
+        # if parsed_log["ClientHost"] == custom_ip:
+        #      print(f"IP: {parsed_log["ClientHost"]} {parsed_log["RequestAddr"]}{parsed_log["RequestPath"]} {parsed_log["DownstreamStatus"]}")
+
         count_by_occurence(parsed_log, "ClientHost", ip_hits)
         count_by_occurence(parsed_log, "DownstreamStatus", statuses_count)
         count_by_occurence(parsed_log, "RequestAddr", request_addr_count)
         count_by_occurence(parsed_log, "RequestPath", request_path_count)
+
+    for status in statuses_count.items():
+        if status[0] >= 500 and status[0] <=599:
+            bad_statuses_count.update({
+                status[0]: status[1]
+            })
+    
 
                 
     show_top_metrics("TOP 10 IP hit counts...",ip_hits)
