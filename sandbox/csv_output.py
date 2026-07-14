80f107d11645e207b9f25d292b6e8e6844ca52fb
o.pawliszak <o.pawliszak@pl.mw-wheels.com>
2026-07-14 13:10:14 +0200

    "added csv output saving"

--- sandbox/csv_output.py
+++ sandbox/csv_output.py
@@ -2,9 +2,15 @@ import csv
 
 def test_csv_output():
 
+    data = {"example.com": "192.168.0.1","example2.com": "192.168.0.2"}
+
     with open('data.csv', 'w', newline="") as f:
-        
+        columns = ["site","address"]
+        writer = csv.DictWriter(f,fieldnames=columns)
+        writer.writeheader()
 
-
-
+        for row in data.items():
+            writer.writerow(
+                {columns[0]: row[0], columns[1]: row[1]}
+            )
 test_csv_output()
